df2394648ab372294ba239bc0ce13549cb7f7c29
nmass17 <windward_student@school.windwardschool.org>
2016-08-30 08:26:57 -0700

    v2

--- src/Class1.java
+++ src/Class1.java
@@ -1,9 +1,9 @@
 
 public class Class1 {
 	public static void main(String[]args){
-		System.out.println("hello");
 		int x;
-		x = 1234;
+		x = 123;
+		System.out.println("hello");
 		System.out.println((x-x%10)/10);
 	}
 
